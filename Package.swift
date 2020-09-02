e0d8dd270d4aa6e8cd4c23e7ae3907ce18add692
nixberg <k.nixberg@gmail.com>
2020-09-02 19:51:00 +0200

    Rename target

--- Package.swift
+++ Package.swift
@@ -4,14 +4,18 @@ import PackageDescription
 
 let package = Package(
     name: "genpass-swift",
+    products: [
+        .executable(name: "genpass", targets: ["Genpass"])
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
     ],
     targets: [
         .target(
-            name: "genpass",
+            name: "Genpass",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser")
-            ])
+            ]
+        )
     ]
 )
