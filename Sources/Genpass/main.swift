97497690d4557f7904a5ce31c0998941b468e72f
nixberg <k.nixberg@gmail.com>
2020-09-04 00:23:53 +0200

    Make securityLevel a Float64

--- Sources/Genpass/main.swift
+++ Sources/Genpass/main.swift
@@ -15,25 +15,31 @@ struct Genpass: ParsableCommand {
     var alternativeOutputStyle: AlternativeOutputStyle?
     
     @Argument(help: "The desired security level.")
-    var securityLevel: UInt = 128
+    var securityLevel: Float64 = 128
     
-    mutating func run() {
+    func validate() throws {
+        guard securityLevel > 0 else {
+            throw ValidationError("Please specify a 'security-level' greater than 0.")
+        }
+    }
+    
+    func run() {
         switch alternativeOutputStyle {
         case nil:
             let characters = "abcdefghijklmnopqrstuvwxyz0123456789"
             let bitsPerCharacter = log2(Float64(characters.count))
-            let characterCount = Int((Float64(securityLevel) / bitsPerCharacter).rounded(.up))
+            let characterCount = Int((securityLevel / bitsPerCharacter).rounded(.up))
             print(String((0..<characterCount).map { _ in characters.randomElement()! }))
             
         case .phrase:
             let bitsPerWord = log2(Float64(words.count))
-            let wordCount = Int((Float64(securityLevel) / bitsPerWord).rounded(.up))
+            let wordCount = Int((securityLevel / bitsPerWord).rounded(.up))
             print((0..<wordCount).map { _ in words.randomElement()! }.joined(separator: "-"))
             
         case .urbit:
             assert(prefixes.count == 256)
             assert(suffixes.count == 256)
-            let wordCount = (securityLevel + 15) / 16
+            let wordCount = Int((securityLevel / 16).rounded(.up))
             print((0..<wordCount).map { _ in
                 prefixes.randomElement()! + suffixes.randomElement()!
             }.joined(separator: "-"))
